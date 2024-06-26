57030da5439ff45ee50b261e2bf16f414a481b9d
Hamada Abdelaziz Mohamed <43202748+admin3651@users.noreply.github.com>
2024-06-26 12:16:21 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// This file has some linting issues
+var unused_variable = "This will be flagged";
+
+function badlyFormattedFunction (x,y){
+    if(x==y)
+        return true;
+    else
+        return false;
+}
+
+console.log("Hello, World!")
