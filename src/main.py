73fee3e9ee90c607ad5b10ff3345f1f271d17798
Hamada Abdelaziz Mohamed <43202748+admin3651@users.noreply.github.com>
2024-06-26 12:16:53 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# This file has some linting issues
+import os, sys
+
+def poorly_formatted_function( arg1,arg2 ):
+    if arg1==arg2:
+        print("They are equal")
+    else:
+        print("They are not equal")
+
+x = 10
+y = 20
+poorly_formatted_function(x,y)
